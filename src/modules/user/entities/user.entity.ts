71791bb4261e2cff7387b028e2c7fe982113c535
舟匪 <zhoufei@youzhuan.onaliyun.com>
2021-11-18 01:00:08 +0800

    Chore 增加common entity

--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,36 +1,9 @@
-import {
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-  CreateDateColumn,
-  VersionColumn,
-} from 'typeorm';
+import { Entity, Column } from 'typeorm';
+
+import { Common } from 'src/common/entity/common.entity';
 
 @Entity()
-export class User {
-  // 主键id
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  // 创建时间
-  @CreateDateColumn()
-  createTime: Date;
-
-  // 更新时间
-  @UpdateDateColumn()
-  updateTime: Date;
-
-  // 软删除
-  @Column({
-    default: false,
-  })
-  isDelete: boolean;
-
-  // 更新次数
-  @VersionColumn()
-  version: number;
-
+export class User extends Common {
   // 昵称
   @Column('text')
   nickname: string;
